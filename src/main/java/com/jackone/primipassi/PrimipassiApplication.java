176ba2e1b5f9420a212b856ef307c1aac607caf5
JackOne850 <118886732+JackOne850@users.noreply.github.com>
2023-10-11 12:45:51 +0200

    Initial commit

---
+++ src/main/java/com/jackone/primipassi/PrimipassiApplication.java
@@ -0,0 +1,13 @@
+package com.jackone.primipassi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimipassiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimipassiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackone/primipassi/PrimipassiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackone.primipassi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimipassiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
